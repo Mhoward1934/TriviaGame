402eb7454bbbc34a73a324d0d85cd8e4f27915c2
Monroe Howard II <monroehowardii@Monroes-MacBook-Air.local>
2018-10-18 02:03:06 -0500

    minor updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,25 +1,51 @@
 var timer = 30
 var display = 00;
 var correctAns = 0;
+var questionAns = qAns;
 var qAns = ["Apple", "Ask Me Anything", "Central Processing Unit", "Twitter", "Super Nintendo Entertainment System"];
 var incorrectAns = 0;
 var score = 0;
+var intervalId;
 
+
+function decrement() {
+    timer--;
+    $("#display").html(timer);
+    if (timer === 0) {
+        stop()
+    }
+}
 //Counts correct and incorrect choices
 function count() {
-    correctAns === qAns;
+    questionAns === qAns;
     correctAns++;
-    document.getElementById("correctAns").innerHTML = Correct;
-    correctAns !== qAns;
-    incorrectAns++;
-    document.getElementById("incorretAns").innerHTML = Incorrect;
+    document.getElementById("correctAns").innerHTML = correctAns;
+    if (questionAns !== qAns) {
+        incorrectAns++;
+        document.getElementById("incorrectAns").innerHTML = incorrectAns;
+    }  
 }
-//Starts the timer
-//function timer() {
-      intervalId = setInterval(decrement, 1000);
-      var decrement = timer--;
-        $("#display").html(display);
-//}
 
-//When the player clicks the start game button the button is hiddent and the timer starts
-//$("button").on(click)
+//When the player clicks the "Start Game" button the button is hidden and the timer starts
+$("button").on("click", function () {
+    intervalId = setInterval(decrement, 1000);
+    timer === 30;
+    $(this).hide();
+    //console.log();
+
+    //Tracking player answers
+    $("#questionAns").on("click", function () {
+        count()
+    })
+
+
+
+
+})
+
+
+
+
+function stop() {
+    clearInterval(intervalId);
+}
