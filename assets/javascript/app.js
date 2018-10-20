3978ecd9a0fe9a0d375987508b12dfa4b80ad096
Monroe Howard II <monroehowardii@Monroes-MacBook-Air.local>
2018-10-19 22:55:55 -0500

    minor updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,11 @@
-var timer = 30
+
+var timer = 30;
 var display = 00;
-var correctAns = 0;
-var question1 = [];
-var qAns = ["Apple", "Ask Me Anything", "Central Processing Unit", "Twitter", "Super Nintendo Entertainment System"];
-var incorrectAns = 0;
+var qAns = ["Apple", "Ask Me Anything", "Central Processing Unit", "Twitter", "Super Nintendo Entertainment System"],
+    data = $("input.value"),
+    correctAns = 0,
+    incorrectAns = 0;
 var intervalId;
-//console.log(question1);
 
 function decrement() {
     timer--;
@@ -15,16 +15,16 @@ function decrement() {
     }
 }
 //Function to track correct and incorrect choices
-function count() {
-    if (questionAns === "Apple") {
-        correctAns++;
-        document.getElementById("correctAns").innerHTML = correctAns;
-    } else {
-        (questionAns !== "Apple");
-        incorrectAns++;
-        document.getElementById("incorrectAns").innerHTML = incorrectAns;
-    }
-}
+//function count() {
+   // if (questionAns === qAns) {
+   //     correctAns++;
+   //     document.getElementById("correctAns").innerHTML = correctAns;
+  //  } else {
+   //     (questionAns !== qAns);
+   //     incorrectAns++;
+  //      document.getElementById("incorrectAns").innerHTML = incorrectAns;
+  //  }
+//}
 
 //When the player clicks the "Start Game" button the button is hidden and the timer starts
 $("button").on("click", function () {
@@ -34,8 +34,22 @@ $("button").on("click", function () {
     //console.log();
 
     //When the player clicks on a radio button the response is collected using the "function count" as outlined above
-    $("#questionAns").on("click", function () {
-        count();
+    $(".data").click(function () {
+        for (var i = 0; i < data.length; i++) {
+
+            if (data[i].checked) {
+                console.log(data);
+                if (qAns.indexOf(data[i].value) !== -1) {
+                    correctAns++;
+                    $("#correctAns").html = correctAns;
+                }
+    
+                else {
+                    incorrectAns++;
+                    $("#incorrectAns").html = incorrectAns;
+                }
+            } 
+        }
     })
 
     //Function "Stop" stops the timer when it reaches zero
