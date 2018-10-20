413d10d7a8aae427f94612d3b62e9fe4c6809a48
Monroe Howard II <monroehowardii@Monroes-MacBook-Air.local>
2018-10-20 17:26:09 -0500

    updated html and js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,7 @@
 var timer = 30;
 var display = 00;
 var qAns = ["Apple", "Ask Me Anything", "Central Processing Unit", "Twitter", "Super Nintendo Entertainment System"],
-    data = $("input.value"),
+    data = $("input"),
     correctAns = 0,
     incorrectAns = 0;
 var intervalId;
@@ -14,17 +14,25 @@ function decrement() {
         stop()
     }
 }
-//Function to track correct and incorrect choices
-//function count() {
-   // if (questionAns === qAns) {
-   //     correctAns++;
-   //     document.getElementById("correctAns").innerHTML = correctAns;
-  //  } else {
-   //     (questionAns !== qAns);
-   //     incorrectAns++;
-  //      document.getElementById("incorrectAns").innerHTML = incorrectAns;
-  //  }
-//}
+//When the player clicks on a radio button the response is collected using the "function count" as outlined above
+function count() {
+    $(".data").on("click", function () {
+        for (var i = 0; i < data.length; i++) {
+
+            if (data[i].checked) {
+                //console.log(form-check-input);
+                if (qAns.indexOf(data[i].value) !== -1) {
+                    correctAns++;
+                    $("#correctAns").html = correctAns;
+                }
+                else {
+                    incorrectAns++;
+                    $("#incorrectAns").html = incorrectAns;
+                }
+            }
+        }
+    })   
+}
 
 //When the player clicks the "Start Game" button the button is hidden and the timer starts
 $("button").on("click", function () {
@@ -32,27 +40,12 @@ $("button").on("click", function () {
     timer === 30;
     $(this).hide();
     //console.log();
+    count()
 
-    //When the player clicks on a radio button the response is collected using the "function count" as outlined above
-    $(".data").click(function () {
-        for (var i = 0; i < data.length; i++) {
+//Hide results until time is up or player is done answering questions
 
-            if (data[i].checked) {
-                console.log(data);
-                if (qAns.indexOf(data[i].value) !== -1) {
-                    correctAns++;
-                    $("#correctAns").html = correctAns;
-                }
-    
-                else {
-                    incorrectAns++;
-                    $("#incorrectAns").html = incorrectAns;
-                }
-            } 
-        }
-    })
 
-    //Function "Stop" stops the timer when it reaches zero
+//Function "Stop" stops the timer when it reaches zero
 })
 function stop() {
     clearInterval(intervalId);
